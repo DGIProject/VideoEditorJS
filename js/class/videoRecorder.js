8b6c5d145581ed66ca36ac1d94a2af8975e9fe46
Willena <villenagui.free.fr@gmail.com>
2014-09-13 17:59:45 +0200

    Audio Recording
    ** Tests + update on the main class

--- js/class/videoRecorder.js
+++ js/class/videoRecorder.js
@@ -1,11 +1,12 @@
 /**
  * Created by Guillaume on 05/08/14.
  */
-VideoRecorder = function(videoId,canvasId,autostart,framesRate){
+VideoRecorder = function(videoId,canvasId,autostart,sound,framesRate){
     //arg[0] -> frameRate
     //this.videoQuerySelector = videoElement;
    // this.canvasQuerySelector = canvasElement;
     this.autostart = autostart | true;
+    this.sound = sound | false;
     canvas = document.getElementById(canvasId);
     ctx = canvas.getContext('2d');
     video = document.getElementById(videoId);
@@ -15,22 +16,143 @@ VideoRecorder = function(videoId,canvasId,autostart,framesRate){
     this.numberImage = 0;
     this.OAjax = [];
     this.sendedImage = 0;
-    this.st = 0
+    this.st = 0;
+
+    this.leftchannel = [];
+    this.rightchannel = [];
+    this.recorder = null;
+    this.recordingLength = 0;
+    this.volume = null;
+    this.audioInput = null;
+    this.audioContext = null;
+    this.context = null;
+
     var that = this;
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
     window.URL = window.URL || window.webkitURL;
-    navigator.getUserMedia({video:true}, function(stream){
+    navigator.getUserMedia({video:true, audio:this.sound}, function(stream){
         video.src = window.URL.createObjectURL(stream);
         localMediaStream = stream;
 
         if (that.autostart){that.startRecording();}
 
+        if (that.sound){
+            console.log('Audio Process');
+            that.audioContext = window.AudioContext || window.webkitAudioContext;
+            that.context = new that.audioContext();
+
+            // creates a gain node
+            that.volume = context.createGain();
+
+            // creates an audio node from the microphone incoming stream
+            that.audioInput = context.createMediaStreamSource(localMediaStream);
+
+            // connect the stream to the gain node
+            that.audioInput.connect(volume);
+
+            /* From the spec: This value controls how frequently the audioprocess event is
+             dispatched and how many sample-frames need to be processed each call.
+             Lower values for buffer size will result in a lower (better) latency.
+             Higher values will be necessary to avoid audio breakup and glitches */
+            var bufferSize = 2048;
+            that.recorder = context.createJavaScriptNode(bufferSize, 2, 2);
+
+            recorder.onaudioprocess = function(e){
+                console.log ('recording');
+                var left = e.inputBuffer.getChannelData (0);
+                var right = e.inputBuffer.getChannelData (1);
+                // we clone the samples
+                that.leftchannel.push (new Float32Array (left));
+                that.rightchannel.push (new Float32Array (right));
+                that.recordingLength += bufferSize;
+            };
+
+            // we connect the recorder
+            that.volume.connect (recorder);
+            that.recorder.connect (context.destination);
+        }
+
     }, this.onCameraFail);
 };
-VideoRecorder.prototype.onCameraSuccess = function(stream){
-   // document.querySelector(this.videoQuerySelector).src = window.URL.createObjectURL(stream);
 
+VideoRecorder.prototype.mergeBuffers = function(channelBuffer, recordingLength){
+    var result = new Float32Array(recordingLength);
+    var offset = 0;
+    var lng = channelBuffer.length;
+    for (var i = 0; i < lng; i++){
+        var buffer = channelBuffer[i];
+        result.set(buffer, offset);
+        offset += buffer.length;
+    }
+    return result;
 };
+
+VideoRecorder.prototype.interleave = function(leftChannel, rightChannel){
+    var length = leftChannel.length + rightChannel.length;
+    var result = new Float32Array(length);
+
+    var inputIndex = 0;
+
+    for (var index = 0; index < length; ){
+        result[index++] = leftChannel[inputIndex];
+        result[index++] = rightChannel[inputIndex];
+        inputIndex++;
+    }
+    return result;
+};
+
+VideoRecorder.prototype.writeUTFBytes = function(view, offset, string){
+    var lng = string.length;
+    for (var i = 0; i < lng; i++){
+        view.setUint8(offset + i, string.charCodeAt(i));
+    }
+};
+
+VideoRecorder.prototype.exportWave = function()
+{
+    // we flat the left and right channels down
+    var leftBuffer = this.mergeBuffers ( this.leftchannel, this.recordingLength );
+    var rightBuffer = this.mergeBuffers ( this.rightchannel, this.recordingLength );
+// we interleave both channels together
+    var interleaved = this.interleave ( leftBuffer, rightBuffer );
+
+// create the buffer and view to create the .WAV file
+    var buffer = new ArrayBuffer(44 + interleaved.length * 2);
+    var view = new DataView(buffer);
+
+// write the WAV container, check spec at: https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+// RIFF chunk descriptor
+    this.writeUTFBytes(view, 0, 'RIFF');
+    view.setUint32(4, 44 + interleaved.length * 2, true);
+    this.writeUTFBytes(view, 8, 'WAVE');
+// FMT sub-chunk
+    this.writeUTFBytes(view, 12, 'fmt ');
+    view.setUint32(16, 16, true);
+    view.setUint16(20, 1, true);
+// stereo (2 channels)
+    view.setUint16(22, 2, true);
+    view.setUint32(24, 44100, true);
+    view.setUint32(28, 44100 * 4, true);
+    view.setUint16(32, 4, true);
+    view.setUint16(34, 16, true);
+// data sub-chunk
+    this.writeUTFBytes(view, 36, 'data');
+    view.setUint32(40, interleaved.length * 2, true);
+
+// write the PCM samples
+    var lng = interleaved.length;
+    var index = 44;
+    var volume = 1;
+    for (var i = 0; i < lng; i++){
+        view.setInt16(index, interleaved[i] * (0x7FFF * volume), true);
+        index += 2;
+    }
+
+// our final binary blob that we can hand off
+    var blob = new Blob ( [ view ], { type : 'audio/wav' } );
+    console.log('exportedWave');
+};
+
 VideoRecorder.prototype.onCameraFail = function (e) {
     console.log('Camera did not work.', e);
 };
@@ -38,11 +160,11 @@ VideoRecorder.prototype.startRecording = function(maxTime){
     var that = this;
 
     this.startTimeOut = window.setTimeout(function(){
-        console.log('StartingRecording ... ')
+        console.log('StartingRecording ... ');
         var time = Math.ceil(1000/that.frameRate);
         that.SnapInterval = window.setInterval(function(){
             if (localMediaStream) {
-                console.log('ImageTaken')
+                console.log('ImageTaken');
                 ctx.drawImage(video,0,0,video.videoWidth,video.videoHeight,0,0,200,150);
                 that.images.push(canvas.toDataURL('image/png'));
               //  that.sendImages(that.numberImage);
@@ -68,13 +190,13 @@ VideoRecorder.prototype.startRecording = function(maxTime){
 VideoRecorder.prototype.stopRecording = function(){
     clearTimeout(this.startTimeOut);
     clearInterval(this.SnapInterval);
+    this.exportWave();
     localMediaStream.stop();
 };
 VideoRecorder.prototype.getImages = function(){
     return this.images;
 };
-VideoRecorder.prototype.sendImages = function(i)
-{
+VideoRecorder.prototype.sendImages = function(i){
     if (i)
     {
         console.log('i');
@@ -128,4 +250,4 @@ VideoRecorder.prototype.sendImages = function(i)
     }
 
 
-}
+};
