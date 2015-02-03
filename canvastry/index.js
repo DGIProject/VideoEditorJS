aec56931ee6b5025458b7067420eecd5a322f2b0
Dylan Delaporte <dylan.delaporte06@gmail.com>
2015-02-03 18:39:09 +0100

    ad properties for tracks

--- canvastry/index.js
+++ canvastry/index.js
@@ -7,7 +7,26 @@ ctx.height = 100;
 ctx.fillStyle = '#E0E0E0';
 ctx.fillRect(0, 0, ctx.width, ctx.height);
 
+var imageThumbnail = new Image();
+
+imageThumbnail.onload = function() {
+    console.log('loaded thumbnail');
+};
+
+imageThumbnail.src = 'http://clangue.net/other/testVideo/canvastry/img/thumbnail.png';
+
+var imageClose = new Image();
+
+imageClose.onload = function() {
+    console.log('loaded close');
+};
+
+imageClose.src = 'http://clangue.net/other/testVideo/canvastry/img/close.png';
+
 var mousedown = false;
+var gap = 0;
+
+var scroll = 0;
 
 var tabElement = [];
 var currentRow = 0;
@@ -69,10 +88,20 @@ c.onmousemove = function(e) {
 
     if(mousedown && currentRow != 'none')
     {
-        tabElement[currentRow].marginLeft = x;
+        tabElement[currentRow].marginLeft = x - gap;
 
         drawElements();
     }
+};
+
+document.getElementById('divCanvas').onscroll = function() {
+    console.log(this.scrollLeft);
+
+    c.style.width = (ctx.width + 5) + 'px';
+    c.style.height = '100px';
+
+    ctx.width = ctx.width + 2;
+    ctx.height = 100;
 };
 
 function rowTabElement(x) {
@@ -85,6 +114,7 @@ function rowTabElement(x) {
             console.log('row : ', i);
 
             currentRow = i;
+            gap = x - tabElement[i].marginLeft;
 
             tabElement[i].selected = true;
         }
@@ -117,10 +147,22 @@ function drawElements() {
     for(var i = 0; i < tabElement.length; i++)
     {
         ctx.beginPath();
-        ctx.lineWidth = '2';
+        ctx.lineWidth = 1;
         ctx.strokeStyle = (tabElement[i].selected) ? 'blue' : 'gray';
         ctx.fillStyle = '#66CCFF;';
-        ctx.rect(tabElement[i].marginLeft, 0, tabElement[i].width, 100);
+        ctx.rect(tabElement[i].marginLeft - scroll, 0, tabElement[i].width, 100);
         ctx.stroke();
+
+        ctx.font = '15px Calibri';
+        ctx.fillStyle = '#000000';
+        ctx.fillText('Test', (tabElement[i].marginLeft + 10) - scroll, 15);
+
+        ctx.drawImage(imageClose, (tabElement[i].marginLeft + tabElement[i].width - 20) - scroll, 5, 15, 15);
+
+        var showWidth = imageThumbnail.width - (((80 - (tabElement[i].width - 20)) / 80) * imageThumbnail.width);
+
+        console.log(showWidth, (80 - (tabElement[i].width - 20)), (((80 - (tabElement[i].width - 20)) / 80) * imageThumbnail.width), imageThumbnail.width);
+
+        ctx.drawImage(imageThumbnail, 0, 0, showWidth, imageThumbnail.height, (tabElement[i].marginLeft + 10), 35, (80 - (80 - (tabElement[i].width - 20))) - scroll, 80 * (imageThumbnail.height / imageThumbnail.width));
     }
 }
