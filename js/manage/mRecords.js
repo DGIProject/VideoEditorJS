1bb9442e49e02ad91ae415ce0b788a53e1552313
Dylan Delaporte <dylan.delaporte06@gmail.com>
2015-03-15 18:04:42 +0100

    add footer

--- js/manage/mRecords.js
+++ js/manage/mRecords.js
@@ -2,29 +2,26 @@
  * Created by Dylan on 10/02/2015.
  */
 
-function newRecord() {
-    document.getElementById('chooseRecordButtons').style.display = '';
-    document.getElementById('videoRecord').style.display = 'none';
-    document.getElementById('saveRecordButton').style.display = 'none';
-    document.getElementById('saveRecordButton').setAttribute('disabled', '');
-
-    $('#recordAudioOrVideoElement').modal('show');
-}
-
-function chooseAudioVideoRecord(audioa){
-
-    audio.style.display = "none";
-    para = {video:true, audio:true};
-    if (audioa)
+function newRecord(type) {
+    if(type == TYPE.VIDEO)
     {
-        para = {video:false,audio:true};
-        video = document.getElementById('audio');
-        audio.style.display = '';
+        eId('audioRecord').style.display = 'none';
+        eId('videoRecord').style.display = 'initial';
+
+        para = {video:true, audio:true};
     }
-    document.getElementById('video').style.display = '';
-    document.getElementById('chooseRecordButtons').style.display = 'none';
-    document.getElementById('videoRecord').style.display = '';
+    else
+    {
+        eId('videoRecord').style.display = 'none';
+        eId('audioRecord').style.display = 'initial';
+
+        para = {video:false,audio:true};
+    }
+    
+    eId('buttonSaveRecord').disabled = true;
 
     startRecordingbtn.disabled = false;
     stopRecordingbtn.disabled = true;
+
+    $('#recordFileModal').modal('show');
 }
