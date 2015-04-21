9baf1a1f5fcc7c17d5352245d35475ffeec02590
Willena <villenagui.free.fr@gmail.com>
2015-04-21 20:51:10 +0200

    Reformat Render

--- js/class/RenderProject.js
+++ js/class/RenderProject.js
@@ -10,39 +10,37 @@ RenderP = function (format) {
     this.commandTracksVideo = [];
     this.commandList = [];
     this.previousZoom = parseInt(document.getElementById('zoomRange').value);
-    changeZoom((parseInt(document.getElementById('zoomRange').max)/2), false);
+    changeZoom((parseInt(document.getElementById('zoomRange').max) / 2), false);
     console.log(currentProject.tabListTracks);
     this.FORMAT = {
-        MPEG4 : { ext : 'mp4', codec : 'mpeg4'},
-        AVI : {ext : 'avi', codec : null},
-        OGG : {ext : 'ogg', codec : null},
-        WEBM : {ext : 'webm', codec : null},
-        TS : {ext : 'ts' , codec : null},
-        X264 : {ext : 'mp4', codec : 'libx264'}
+        MPEG4: {ext: 'mp4', codec: 'mpeg4'},
+        AVI: {ext: 'avi', codec: null},
+        OGG: {ext: 'ogg', codec: null},
+        WEBM: {ext: 'webm', codec: null},
+        TS: {ext: 'ts', codec: null},
+        X264: {ext: 'mp4', codec: 'libx264'}
     };
 
     this.userFormat = format || this.FORMAT.MPEG4;
     console.log(this.userFormat);
 
     var tabVideoTrack = [];
-    for (i=0;i<currentProject.tabListTracks.length;i++)
-    {
-        if (currentProject.tabListTracks[i].type == TYPE.VIDEO)
-        {
+    for (i = 0; i < currentProject.tabListTracks.length; i++) {
+        if (currentProject.tabListTracks[i].type == TYPE.VIDEO) {
             tabVideoTrack.push(currentProject.tabListTracks[i]);
-            console.log("vid",i);
+            console.log("vid", i);
         }
     }
-    console.log("Video",tabVideoTrack.length, tabVideoTrack);
+    console.log("Video", tabVideoTrack.length, tabVideoTrack);
 
-   /* for (i=0;i<tabTracks.length;i++)
-    {
-        trackData = this.sortAllTracks(tabTracks[i].data);
-        console.log("sorted : ", trackData);
+    /* for (i=0;i<tabTracks.length;i++)
+     {
+     trackData = this.sortAllTracks(tabTracks[i].data);
+     console.log("sorted : ", trackData);
 
-    }*/
+     }*/
 
-   this.makeSingleVideoTrack(tabVideoTrack);
+    this.makeSingleVideoTrack(tabVideoTrack);
 
 };
 
@@ -54,61 +52,57 @@ RenderP.prototype.makeSingleVideoTrack = function (tabTrack) {
     //tabTrack.shift();
     console.log(tabTrack);
     blackTab = [];
-    for (t=0;t<tabtracks.length;t++) {
+    t = 0;
+    tabtracks[t].tabElements.sort(function (a, b) {
+        return a.marginLeft - b.marginLeft
+    });
 
-        tabtracks[t].tabElements.sort(function (a, b) {
-            return a.marginLeft - b.marginLeft
-        });
-
-        for (e = 0; e < tabtracks[t].tabElements.length; e++) {
-            if (e == 0) {
-                console.log("0 -> deb");
-                if (tabtracks[t].tabElements[e].marginLeft >= oneSecond) {
-                    console.log("blackFirstTime");
-                    console.log("black from 0 to" + tabtracks[t].tabElements[e].marginLeft);
-                    blackTab.push({'from': 0, 'to': tabtracks[t].tabElements[e].marginLeft});
-                }
-                value = this.checkBlack(tabtracks[t], e);
-                console.log(value);
-                if (value.code)
-                    blackTab.push({'from': value.from, 'to': value.to});
+    for (e = 0; e < tabtracks[t].tabElements.length; e++) {
+        if (e == 0) {
+            console.log("0 -> deb");
+            if (tabtracks[t].tabElements[e].marginLeft >= oneSecond) {
+                console.log("blackFirstTime");
+                console.log("black from 0 to" + tabtracks[t].tabElements[e].marginLeft);
+                blackTab.push({'from': 0, 'to': tabtracks[t].tabElements[e].marginLeft});
             }
-            else if (e == (tabtracks[t].tabElements.length - 1)) {
-                console.log("blackEnd");
-            }
-            else {
-                console.log("BlackOthers");
-                value = this.checkBlack(tabtracks[t], e);
-                console.log(value);
-                if (value.code)
-                    blackTab.push({'from': value.from, 'to': value.to});
-            }
+            value = this.checkBlack(tabtracks[t], e);
+            console.log(value);
+            if (value.code)
+                blackTab.push({'from': value.from, 'to': value.to});
+        }
+        else if (e == (tabtracks[t].tabElements.length - 1)) {
+            console.log("blackEnd");
+        }
+        else {
+            console.log("BlackOthers");
+            value = this.checkBlack(tabtracks[t], e);
+            console.log(value);
+            if (value.code)
+                blackTab.push({'from': value.from, 'to': value.to});
         }
     }
     console.log(blackTab)
 
 };
-RenderP.prototype.checkBlack = function (tabtrack, elementIndex)
-{
-    tempIndex = elementIndex  ;
+RenderP.prototype.checkBlack = function (tabtrack, elementIndex) {
+    tempIndex = elementIndex;
     tempIndex++;
     console.log(tempIndex);
     currentElement = tabtrack.tabElements[elementIndex];
 
     if (!(tempIndex >= tabtrack.tabElements.length)) {
         var nextElement = tabtrack.tabElements[tempIndex];
-        if (nextElement.marginLeft == (currentElement.marginLeft + currentElement.width) || (nextElement.marginLeft - (currentElement.marginLeft + currentElement.width)) < oneSecond/2) {
-            return {code:false}
+        if (nextElement.marginLeft == (currentElement.marginLeft + currentElement.width) || (nextElement.marginLeft - (currentElement.marginLeft + currentElement.width)) < oneSecond / 2) {
+            return {code: false}
         }
         else {
-            return {code:true, from : (currentElement.marginLeft + currentElement.width) , to: nextElement.marginLeft };
+            return {code: true, from: (currentElement.marginLeft + currentElement.width), to: nextElement.marginLeft};
         }
     }
 };
 
-RenderP.prototype.sortAllTracks = function (tabTrack){
-    for (i=0;i<tabTrack.length;i++)
-    {
+RenderP.prototype.sortAllTracks = function (tabTrack) {
+    for (i = 0; i < tabTrack.length; i++) {
         tabTrack[i].tabElements.sort(function (a, b) {
             return a.marginLeft - b.marginLeft
         });
@@ -165,7 +159,7 @@ RenderP.prototype.addBlackV = function (e) {
     var cmd = "";
     if (!(tempIndex > this.elementInTrack.length)) {
         this.nextElement = this.elementInTrack[tempIndex];
-        if (this.nextElement.marginLeft == this.elementEnd || this.nextElement.marginLeft - this.elementEnd < oneSecond/2) {
+        if (this.nextElement.marginLeft == this.elementEnd || this.nextElement.marginLeft - this.elementEnd < oneSecond / 2) {
             //fileContent += "\n element+1 sticked !";
             console.log("sticked");
         }
@@ -185,13 +179,13 @@ RenderP.prototype.addBlackA = function (e) {
     tempIndex++;
     if (!(tempIndex > this.elementInTrack.length)) {
         this.nextElement = this.elementInTrack[tempIndex];
-        if (this.nextElement.marginLeft == this.elementEnd || this.nextElement.marginLeft - this.elementEnd < oneSecond/2) {
+        if (this.nextElement.marginLeft == this.elementEnd || this.nextElement.marginLeft - this.elementEnd < oneSecond / 2) {
             //fileContent += "\n element+1 sticked !";
             console.log("sticked");
         }
         else {
             console.log("black from ", this.elementEnd, "to ", this.nextElement.marginLeft);
-            var cmd  = "-ar 48000 -f s16le -acodec pcm_s16le -ac 2 -i /dev/zero -acodec libmp3lame -aq 4 -t " + Math.ceil((this.nextElement.marginLeft - this.elementEnd) / oneSecond) + " -y " + this.commands[this.t].length + ".mp3";
+            var cmd = "-ar 48000 -f s16le -acodec pcm_s16le -ac 2 -i /dev/zero -acodec libmp3lame -aq 4 -t " + Math.ceil((this.nextElement.marginLeft - this.elementEnd) / oneSecond) + " -y " + this.commands[this.t].length + ".mp3";
             this.commands[this.t].push(cmd);
             this.commandList.push(cmd);
         }
